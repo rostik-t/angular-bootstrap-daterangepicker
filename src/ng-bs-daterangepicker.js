7a42d041c5ac38d3d8d7eb8f932c95c6105fec83
Konstantin Yakushev <kojo@kojo.ru>
2013-10-16 12:35:43 +0400

    INCR version to 0.1.0
    Everything seems stable

--- src/ng-bs-daterangepicker.js
+++ src/ng-bs-daterangepicker.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-bs-daterangepicker-plus v0.0.3
+ * @license ng-bs-daterangepicker-plus v0.1.0
  * (c) 2013 Konstantin Yakushev http://github.com/kojoru/ng-bs-daterangepicker
  * Originally by Luis Farzati
  * License: MIT
